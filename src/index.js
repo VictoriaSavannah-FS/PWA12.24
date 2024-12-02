2e72bc7bbf448a57c41256e808f361191d3e268a
Savannah Victoria <savannahcv@Savannahs-MacBook-Pro.local>
2024-12-02 15:23:04 -0600

    editing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 import "./style.scss";
 
 import carData from "./data.json";
-import { Generator } from "webpack";
+
+// import { Generator } from "webpack"; --> I figured it out! I can't spell properly so when i mistyped-generateX and typed "generator" -> it automatically imported
 
 console.log(carData);
 
@@ -15,11 +16,6 @@ class CarSearch {
     this.years = this.getCarYears(); // fetching years
     //MAKE MAP | links year to make types
     this.makeMap = this.createYrsMakeMap(); // Year|Manufacturer Mapping
-    /*MODELs MAP| Nested Map* 
-        will group nodels by
-        - year | Make
-        - yaer | Make  => Model 
-    */
     this.modelMap = this.createModelMap(); //YEar|Make|model=> Mappins
   }
 
@@ -59,25 +55,23 @@ class CarSearch {
       //destructuting properties**
       //loopthrough
       if (!map.has(year)) {
-        // new Set --> year=Key -> next Map = "value"
         map.set(year, new Map());
-        //ensures maoe has Year" value ! creates new map for Year
+        //ensures map has Year" value ! creates new map for Year // new Set year=Key|next Map = "value"
       }
       // New Map: store the Manufacturers and retrieves "YEAR" MAP| selected Year
-      const manufacturerMap = map.get(year); //Map[| MAnufture(keys) : Set(Models) = "values"
+      const manufacturerMap = map.get(year); //Mapss| MAnufture(keys) : Set(Models) = "values"
 
       // cheks if manufacturer in the Map| If NOt -> initialize new Set wiht it
       if (!manufacturerMap.has(manufacturer)) {
-        //If not--> will add new Model to Set
         manufacturerMap.set(manufacturer, new Set());
+        //If not--> will add new Model to Set
       }
-
       // adds Model tot Manufatire Set
       manufacturerMap.get(manufacturer).add(model); //adds model to set| model->Make->Year [Relationship**] took a while to wrap my mind aroudnthis one, but i think i got it now..
     });
-
     return map; // Return the completed Map
   }
+
   /* Change Dropdown values -> based on data passed || HTML*/
 
   // ----- YEAR Dropdown ----
@@ -89,15 +83,13 @@ class CarSearch {
         .join("")
     );
   }
-
   // ----- MAKE (Manufacturer) Dropdown ----
-
   makeDropdown(selectedYear) {
     const manufacturers = this.makeMap.get(parseInt(selectedYear)) || [];
-    //fetching SEt of manufact. fro Year selected --> If non existent--> empty array*
+    //fetching Set of manufact. for Year selected | If non existent--> empty array*
     return (
       [...manufacturers]
-        //coverting SEts to array --> Map through
+        //coverting Set to array -> Map through
         .map(
           (manufacturer) =>
             `<option value="${manufacturer}">${manufacturer}</option>`
@@ -106,17 +98,14 @@ class CarSearch {
     );
   }
   // ----- MODEL Dropdown ----
-
   modelDropdown(selectedYear, selectedMake) {
     const makeMap = this.modelMap.get(parseInt(selectedYear)); //getting the years from nested maps
     let models = [];
     /* --- Logic *** overview| goal --
-    1. checks if yearData exists 
-    2. chskc for slectedMAke data
-    3. if they exist --> push them to modesl [] array 
+    1. checks if yearData exists| 2. chskc for slectedMAke data|3. if they exist --> push them to models [] array 
     */
     if (makeMap && makeMap.has(selectedMake)) {
-      //Conditonal=> Map ->has selectd make/manufacturer
+      //Conditonal=> Map has selectd make/manufacturer
       models = [...makeMap.get(selectedMake)];
       //fetches data of models for selected make
       //conversts Set to array ->to loop through
@@ -125,8 +114,18 @@ class CarSearch {
       .map((model) => `<option value="${model}">${model}</option>`)
       .join("");
   }
+  //ACTUALLY fetching the Data based on Params*
+  getCarParams(selectedYear, selectedMake, selectedModel) {
+    return this.carData.find(
+      (car) =>
+        car.year === parseInt(selectedYear) &&
+        car.manufacturer === selectedMake &&
+        car.model === selectedModel
+    );
+  }
 }
-//----- HTML --> Dynaics | Event Lsitenrs
+
+//----- HTML --> Dynamics | Event Listeners ----------------
 
 function startCarSearch(carData) {
   //"instantiate" CarSearch class| passes carData
@@ -169,16 +168,16 @@ function startCarSearch(carData) {
     const selectedYear = yearOptions.value; //fetch selected "Year" drpdwn
     const selectedMake = makeOptions.value; // fetch "MAKE" selectx
     //logic for MakeOptions | NEeded params
-    // ensures "YEAR"|"MAKE" optxs selected | IF NOT! CLEARS and disables  MODEL options
+
     if (selectedMake) {
       // fectching | Models based on Make and w/ Year param
       const models = carSearch.modelDropdown(selectedYear, selectedMake);
       //passing data to "modelOptins" dprDwn*
       modelOptions.innerHTML = `<option>Select Model</option>` + models;
-      modelOptions.disabled = false; //Enables--> Dropdwn
+      modelOptions.disabled = false;
     } else {
       modelOptions.innerHTML = `<option value="">Select Model</option>`;
-      modelOptions.disabled = true; // ensures NO dropdown
+      modelOptions.disabled = true;
     }
   });
 }
